61fc7806c967c0cb2b9782dcf5a255c931b9c54b
何春霖 <hcl1687@gmail.com>
2018-01-30 23:10:12 +0800

    unit test

--- test/misc.spec.js
+++ test/misc.spec.js
@@ -1,6 +1,5 @@
 // test cases for edge cases & bug fixes
 import Vue from 'src'
-var _ = Vue.util
 
 describe('Misc', function () {
   it('should handle directive.bind() altering its childNode structure', function () {
@@ -38,18 +37,6 @@ describe('Misc', function () {
     expect(el.innerHTML.replace(xmlns, '')).to.equal('<svg><text>1</text><text>2</text><text>3</text></svg>')
   })
 
-  it('handle interpolated textarea', function (done) {
-    var el = document.createElement('div')
-    el.innerHTML = '<textarea>hello {{msg}}</textarea>'
-    var vm = new Vue({
-      el: el,
-      data: {
-        msg: 'test'
-      }
-    })
-    expect(el.innerHTML).to.equal('<textarea>hello test</textarea>')
-  })
-
   it('prefer bound attributes over static attributes', () => {
     var el = document.createElement('div')
     el.id = 'test'
@@ -85,95 +72,26 @@ describe('Misc', function () {
     expect(ret).to.be.true
   })
 
-  // #2445
-  it('fragment attach hook should check if child is inDoc', function (done) {
-    var el = document.createElement('div')
-    document.body.appendChild(el)
-    var spyParent = jasmine.createSpy('attached parent')
-    var spyChild = jasmine.createSpy('attached child')
-
-    new Vue({
-      el: el,
-      template: '<comp v-for="n in 1"></comp>',
-      components: {
-        comp: {
-          template: '<div><child></child></div>',
-          attached: function () {
-            expect(_.inDoc(this.$el)).toBe(true)
-            spyParent()
-          },
-          activate: function (next) {
-            setTimeout(function () {
-              next()
-              check()
-            }, 100)
-          },
-          components: {
-            child: {
-              template: 'foo',
-              attached: spyChild
-            }
-          }
-        }
-      }
-    })
-
-    function check () {
-      expect(spyParent).toHaveBeenCalled()
-      expect(spyChild).toHaveBeenCalled()
-      done()
-    }
-  })
-
   // #2500
   it('template parser tag match should include hyphen', function () {
+    var el = document.createElement('div')
+    el.innerHTML = '<div>{{{ test }}}</div>'
     var vm = new Vue({
-      el: document.createElement('div'),
-      template: '<div>{{{ test }}}</div>',
+      el: el,
       data: {
         test: '<image-field></image-field>'
       }
     })
-    expect(vm.$el.querySelector('image-field').namespaceURI).not.toMatch(/svg/)
+    expect(vm.$el.querySelector('image-field').namespaceURI).to.not.include('svg')
   })
 
   // #2657
   it('template v-for with v-if', function () {
+    var el = document.createElement('div')
+    el.innerHTML = '<div><div v-for="n in 6" v-if="n % 2">{{ n }}</div></div>'
     var vm = new Vue({
-      el: document.createElement('div'),
-      template: '<div><template v-for="n in 6" v-if="n % 2">{{ n }}</template></div>'
+      el: el
     })
-    expect(vm.$el.textContent).toBe('135')
-  })
-
-  // #2821
-  it('batcher should keep flushing until all queues are depleted', function (done) {
-    var spy = jasmine.createSpy()
-    var vm = new Vue({
-      el: document.createElement('div'),
-      template: '<test :prop="model"></test>',
-      data: {
-        model: 0,
-        count: 0
-      },
-      watch: {
-        count: function () {
-          this.model++
-        }
-      },
-      components: {
-        test: {
-          props: ['prop'],
-          watch: {
-            prop: spy
-          }
-        }
-      }
-    })
-    vm.count++
-    Vue.nextTick(function () {
-      expect(spy).toHaveBeenCalled()
-      done()
-    })
+    expect(vm.$el.textContent).to.equal('135')
   })
 })
